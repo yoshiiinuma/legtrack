2cac1093420a3a52eedda9dafa3427e721282f51
Yoshiaki Iinuma <yoshiaki.iinuma@hawaii.gov>
2017-12-15 08:10:03 -1000

    Changed create db scripts to accept environment arg

--- drop-remote-mysql-db.php
+++ drop-remote-mysql-db.php
@@ -5,8 +5,17 @@ namespace legtrack;
 require_once 'lib/functions.php';
 require_once 'lib/remote_mysql.php';
 
-$env = 'development';
+function usage($argv) {
+  echo "\nUASGE: php drop-remote-mysql.php <env>\n\n";
+  echo "  env: development|test|production\n";
+}
 
+if ($argc < 1 || $argc > 2) {
+  usage($argv);
+  exit();
+}
+
+$env = $argv[1] || 'development';
 loadEnv($env);
 
 $db = new RemoteMysql();
